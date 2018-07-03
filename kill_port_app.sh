4d14db382c283f41bfd9dc5e9cdc37cc5ef50d39
Ashit Kumar <kmr.ashit1303@gmail.com>
2018-07-03 15:07:11 +0530

    Update kill_port_app.sh

--- kill_port_app.sh
+++ kill_port_app.sh
@@ -2,11 +2,12 @@
 
 #colors
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-BLUE='\033[0;34m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+BLUE='\033[1;34m'
+
 
 echo -e "${BLUE}
  _  _____ _     _          _    ____  ____  
@@ -17,16 +18,13 @@ echo -e "${BLUE}
 "
 
 #check that user must be root
-
+echo -ne "${PURPLE}Developed by ASHIT KUMAR Report bugs to kmr.ashit1303@gmail.com\n"
 function ROOT_CHECK {
-
 if [ "$EUID" -eq 0 ]
 then    
-	SECS=$((1 * 5))
+	SECS=$((1 * 3))
 	while [ $SECS -gt 0 ]; do
-       echo -ne "${PURPLE}Developed by ASH"
        echo -ne "${RED}Starting Application... $SECS\033[0K\r"
-       
 	   sleep 1
 	   : $((SECS--))
 	done
@@ -38,14 +36,21 @@ fi
 
 ROOT_CHECK
 
-function KILL_PORT(){
 echo -e "${CYAN}This application allow you to kill application using the network port you want to use. 
 This might kill applicaiton which is in use.
-${RED}USE IT ON YOUR OWN RISK"
-read -p "Enter the port you want to check for application:  " PORT_NO
-sudo netstat -nlp | grep :${PORT_NO}
+${RED}\n<--- USE IT ON YOUR OWN RISK --->\n<--- USE 'ctrl+c' TO EXIT ---> "
 
-read -p "Sure to kill listed applications ? { yes | no }:  " REPLY
+function KILL_PORT(){
+echo -e "${BLUE}"
+read -p "Enter the port you want to check for application:  " PORT_NO
+netstat -nlp | grep :${PORT_NO}
+if [ "$?" -eq 0 ]
+then 
+    read -p "Sure to kill listed applications ? { yes | no }:  " REPLY
+else 
+    echo -e "${RED}No process is running on port ${PORT_NO}\nPlease try again"
+    KILL_PORT
+fi
 
 if [ $REPLY == "yes" ]
 then
@@ -54,26 +59,11 @@ PID="$(ss -lptn sport = :${PORT_NO} | grep LISTEN | cut -d "=" -f 2 | cut -d ","
 echo -e "${CYAN}Killing ${PID}"
 kill -9 $PID && echo -e "${RED}Process killed" ||echo -e "${PURPLE}Failed to kill process"
 else
-echo -e "${RED}Aborted"
-exit 0
+echo -e "${RED}\nAborted"
+KILL_PORT
+exit 1
 fi
 return 0
 }
 
 KILL_PORT
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
