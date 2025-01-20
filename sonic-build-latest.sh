6c7e7c4dddcb8aa43e36fc9b9a97f90fb9c18ddc
Ashit Kumar <kmr.ashit1303@gmail.com>
2025-01-20 21:07:50 +0530

    Update sonic-build-latest.sh

--- sonic-build-latest.sh
+++ sonic-build-latest.sh
@@ -1,123 +1,81 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/sh
 
-##
-#  Sonic
-#
-#  Fast, lightweight and schema-less search backend
-#  Copyright: 2023, Valerian Saliou <valerian@valeriansaliou.name>
-#  License: Mozilla Public License v2.0 (MPL v2.0)
-##
+# SETUP POINTLESS REPO
 
-# Detect system architecture
-detect_architecture() {
-    arch=$(uname -m)
-    os=$(uname -s | tr '[:upper:]' '[:lower:]')
-    case $arch in
-        aarch64)
-            if [[ "$os" == "mingw"* || "$os" == "cygwin"* ]]; then
-                TARGET_ARCH="aarch64-pc-windows-msvc"
-            else
-                TARGET_ARCH="aarch64-linux-android"
-            fi
-            ;;
-        x86_64)
-            if [[ "$os" == "mingw"* || "$os" == "cygwin"* ]]; then
-                TARGET_ARCH="x86_64-pc-windows-msvc"
-            else
-                TARGET_ARCH="x86_64-unknown-linux-gnu"
-            fi
-            ;;
-        armv7l)
-            TARGET_ARCH="armv7-unknown-linux-gnueabihf"
-            ;;
-        i686)
-            if [[ "$os" == "mingw"* || "$os" == "cygwin"* ]]; then
-                TARGET_ARCH="i686-pc-windows-msvc"
-            else
-                TARGET_ARCH="i686-unknown-linux-gnu"
-            fi
-            ;;
-        *)
-            echo "Unsupported architecture or OS: $arch on $os"
-            exit 1
-            ;;
-    esac
-    echo "Detected architecture: $arch on $os -> $TARGET_ARCH"
-}
+# Update and install required packages
+apt-get update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install coreutils gnupg apt-transport-https
 
-# Read arguments
-while [ "$1" != "" ]; do
-    argument_key=$(echo $1 | awk -F= '{print $1}')
-    argument_value=$(echo $1 | awk -F= '{print $2}')
-    case $argument_key in
-        -v | --version)
-            # Notice: strip any leading 'v' to the version number
-            SONIC_VERSION="${argument_value/v/}"
-            ;;
-        *)
-            echo "Unknown argument received: '$argument_key'"
-            exit 1
-            ;;
-    esac
+# Create sources.list.d directory if it doesn't exist
+mkdir -p $PREFIX/etc/apt/sources.list.d
 
-    shift
-done
-
-# Ensure release version is provided
-if [ -z "$SONIC_VERSION" ]; then
-  echo "No Sonic release version was provided, please provide it using '--version'"
-  exit 1
-fi
-
-# Define release pipeline
-function release_for_architecture {
-    final_tar="v$SONIC_VERSION-$1-$2.tar.gz"
-
-    rm -rf ./sonic/ && \
-        cargo build --target "$3" --release && \
-        mkdir ./sonic && \
-        if [[ "$3" == *"windows-msvc"* ]]; then
-            cp -p "target/$3/release/sonic.exe" ./sonic/ 
-        else
-            cp -p "target/$3/release/sonic" ./sonic/
-        fi
-        cp -r ./config.cfg sonic/ && \
-        tar --owner=0 --group=0 -czvf "$final_tar" ./sonic && \
-        rm -r ./sonic/
-    release_result=$?
-
-    if [ $release_result -eq 0 ]; then
-        echo "Result: Packed architecture: $1 ($2) to file: $final_tar"
-    fi
-
-    return $release_result
-}
-
-# Detect architecture
-detect_architecture
-
-
-# Clone Sonic repository if not already present
-REPO_URL="https://github.com/valeriansaliou/sonic"
-if [ ! -d "sonic" ]; then
-    echo "Cloning Sonic repository from $REPO_URL..."
-    git clone "$REPO_URL"
-fi
-
-# Change directory to the Sonic repository
-pushd sonic > /dev/null
-
-rc=0
-
-echo "Executing release steps for Sonic v$SONIC_VERSION..."
-release_for_architecture "$TARGET_ARCH" "gnu" "$TARGET_ARCH"
-rc=$?
-
-if [ $rc -eq 0 ]; then
-    echo "Success: Done executing release steps for Sonic v$SONIC_VERSION"
+# Write the appropriate source file based on the repository policy
+if apt-cache policy | grep -q "termux.*24\|termux.org\|bintray.*24\|k51qzi5uqu5dg9vawh923wejqffxiu9bhqlze5f508msk0h7ylpac27fdgaskx"; then
+  echo "deb https://its-pointless.github.io/files/24 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 else
-    echo "Error: Failed executing release steps for Sonic v$SONIC_VERSION"
+  echo "deb https://its-pointless.github.io/files/21 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 fi
-popd > /dev/null
 
-exit $rc
+# Add the signing key from the pointless repository
+if command -v curl > /dev/null; then
+  curl -sLo $PREFIX/etc/apt/trusted.gpg.d/pointless.gpg --create-dirs https://its-pointless.github.io/pointless.gpg
+elif command -v wget > /dev/null; then
+  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://its-pointless.github.io/pointless.gpg
+fi
+
+# Update apt
+apt update
+
+# Install development tools
+pkg install -y rust libgcc build-essential binutils make
+
+# Build Sonic
+export RUSTFLAGS="-C link-args=-Wl,--allow-multiple-definition"
+export TARGET_ARCH="aarch64-linux-android"
+
+GIT_REPO = https://github.com/valeriansaliou/sonic.git
+# Ensure Git repository is provided
+
+# Extract project name from Git repository URL
+project_name=$(basename "$GIT_REPO" .git)
+echo "Project: $project_name"
+
+# Clone repository if not already cloned
+if [ ! -d "$project_name" ]; then
+  echo "Cloning repository from $GIT_REPO..."
+  git clone "$GIT_REPO"
+fi
+
+# Navigate to the project directory
+pushd "$project_name" > /dev/null
+
+# Define the release function
+release_for_architecture() {
+  local target_dir="target/$TARGET_ARCH/release"
+  local final_tar="v${project_name}.tar.gz"
+
+  cargo build --release --target "$TARGET_ARCH"
+  mkdir -p release_output
+  cp -r ./config.cfg release_output
+  cp -p "$target_dir/$project_name" release_output/
+  tar --owner=0 --group=0 -czvf "$final_tar" -C release_output .
+  local release_result=$?
+
+  if [ $release_result -eq 0 ]; then
+    echo "Successfully packed: $final_tar"
+  else
+    echo "Error: Packing failed."
+  fi
+
+  return $release_result
+}
+
+# Execute the release process
+echo "Building and packaging for $TARGET_ARCH..."
+release_for_architecture
+build_result=$?
+
+# Clean up and exit
+popd > /dev/null
+exit $build_result
